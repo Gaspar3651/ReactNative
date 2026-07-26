7095e889f483a28d6d9fb8e2318b86fad13079d7
gaspar3651-pessoal <andrielysson.barbosa@gmail.com>
2026-07-26 07:57:24 -0300

    - Constutor - State - Metodos

--- Apendendo a Criar Apps/ExpoGo/meuapp01/App.js
+++ Apendendo a Criar Apps/ExpoGo/meuapp01/App.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			nome: 'Andrielysson'
+		};
+
+		this.entrar = this.entrar.bind(this);
+	}
+
+	entrar(nome){
+		if (this.state.nome == 'Andrielysson') {
+			this.setState({
+				nome: nome
+			});
+		} else {
+			this.setState({
+				nome: 'Andrielysson'
+			});
+		}
+	}
+
 	render(){
-		
 		let nome = 'Andrielysson';
 		let img = 'https://sujeitoprogramador.com/steve.png';
 		
 		return(
 			<View>
 				<Text style={{marginTop: 100, fontSize: 25}}>Olá Mundo!!!!</Text>
-				<Text>Meu primeiro App</Text>
-				<Text>Batata!</Text>
-				<Text style={{ color: 'red', fontSize: 25, margin: 15 }}>
+				<Text style={{fontSize: 25}}>Meu primeiro App</Text>
+
+				<Button
+					title='Mudar nome do state'
+					onPress={ () => this.entrar('Batata') }
+				/>
+
+				<Text 
+					style={{
+						fontSize: 25,
+						color: 'red',
+						textAlign: 'center'
+					}}
+				>
+					Nome com state: {this.state.nome}
+				</Text>
+
+				<Text 
+					style={{
+						fontSize: 25,
+						marginTop: 15,
+						marginLeft: 15
+					}}
+				>
 					Sujeito Programador: {nome}
 				</Text>
 
@@ -38,6 +79,7 @@ class Jobs extends Component {
 						width: this.props.largura, 
 						height: this.props.altura 
 					}}
+					onPress={ () => this.entrar('Batata') }
 					/>
 			</View>
 		);
