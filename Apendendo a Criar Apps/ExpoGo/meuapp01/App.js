3f11940b0492fca2be8783ba4a009ed85e0f5302
gaspar3651-pessoal <andrielysson.barbosa@gmail.com>
2026-07-27 19:39:28 -0300

    inputs

--- Apendendo a Criar Apps/ExpoGo/meuapp01/App.js
+++ Apendendo a Criar Apps/ExpoGo/meuapp01/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, Image, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Image, Button, StyleSheet } from 'react-native';
 class App extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			nome: 'Andrielysson',
-			btn: 'Batata'
+			nome: '',
+			inputNome: '',
+			numero: ''
 		};
 
 		this.mudarNome = this.mudarNome.bind(this);
+		this.formatarNumero = this.formatarNumero.bind(this);
+		this.entrar = this.entrar.bind(this);
+	}
+
+	render(){
+		let img = 'https://sujeitoprogramador.com/steve.png';
+		let nameBtn = 'Mudar nome do state para ' + this.state.btn;
+		
+		return(
+			<View style={styles.container} >
+				<Header/>
+
+				<View style={[styles.body]}>
+					<Text style={{fontSize: 25}}>Olá Mundo !!!!</Text>
+					<Text style={{fontSize: 25}}>Meu primeiro App</Text>
+
+
+					<TextInput style={styles.input} placeholder='Digite seu nome...' onChangeText={(texto) => {this.setState({inputNome: texto})}}/>
+					<TextInput style={styles.input} placeholder='Digite seu número...' onChangeText={this.formatarNumero}/>
+					<Button title='Entrar' onPress={ () => this.entrar() }/>
+
+					<Text>{this.state.nome}</Text>
+					<Text>{this.state.numero}</Text>
+					
+					<Jobs largura={400} altura={400} nome="Steve Jobs"/>
+				</View>
+				
+				<Footer/>
+			</View>
+		);
 	}
 
 	mudarNome(nome){
@@ -18,38 +49,50 @@ class App extends Component {
 		});
 	}
 
-	render(){
-		let img = 'https://sujeitoprogramador.com/steve.png';
-		let nameBtn = 'Mudar nome do state para ' + this.state.btn;
+	entrar(){
+		if (this.state.inputNome === '') {
+			alert('Digite seu nome!');
+			return;
+		}
 		
-		return(
-			<View style={styles.container} >
-				<View style={[styles.header]}>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 01</Text>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 02</Text>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 03</Text>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 04</Text>
-				</View>
-
-				<View style={[styles.body]}>
-					<Text style={{fontSize: 25}}>Olá Mundo !!!!</Text>
-					<Text style={{fontSize: 25}}>Meu primeiro App</Text>
-
-					<Button title={nameBtn} onPress={ () => this.mudarNome('Batata') }/>
-					<Text  style={{ fontSize: 25, marginTop: 15, marginLeft: 15 }} >Sujeito Programador: {this.state.nome}</Text>
-
-					<Jobs largura={400} altura={400} nome="Steve Jobs"/>
-				</View>
-				
-				<View style={[styles.footer]}>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 01</Text>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 02</Text>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 03</Text>
-					<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 04</Text>
-				</View>
-			</View>
-		);
+		this.setState({
+			nome: 'Bem Vindo: ' + this.state.inputNome
+		});
 	}
+
+	formatarNumero(texto){
+		// Remove tudo que não for número
+		texto = texto.replace(/[^0-9]/g, '');
+		// Limita a 11 dígitos
+		texto = texto.substring(0, 11);
+
+		if (texto.length <= 2 && texto.length > 0) {
+			texto = texto.replace(/^(\d*)/, '($1');
+
+		} else if (texto.length <= 6) {
+			texto = texto.replace(/^(\d{2})(\d+)/, '($1) $2');
+
+		} else if (texto.length < 10) {
+			texto = texto.replace(/^(\d{2})(\d{4})(\d+)/, '($1) $2-$3');
+		
+		} else if (texto.length === 10) {
+			// TELEFONE FIXO => (11) 3456-7890
+			texto = texto.replace(/^(\d{2})(\d{4})(\d{4})$/, '($1) $2-$3');
+		
+		} else if (texto.length === 11) {
+			// TELEFONE MOVEL => (11) 9 8765-4321
+			texto = texto.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})$/, '($1) $2 $3-$4');
+		
+		} else {
+			texto = texto.replace(/^(\d{2})(\d+)/, '($1) $2');
+		}
+
+		this.setState({
+			numero: texto
+		});
+	}
+
+	
 }
 
 class Jobs extends Component {
@@ -64,6 +107,31 @@ class Jobs extends Component {
 					style={{ width: this.props.largura, height: this.props.altura, margin: 'auto' }}
 					onPress={ () => this.entrar('Batata') }
 				/>
+			</View>
+		);
+	}
+}
+
+class Footer extends Component{
+	render(){
+		return(
+			<View style={[styles.footer]}>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 01</Text>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 02</Text>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 03</Text>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 04</Text>
+			</View>
+		);
+	}
+}
+class Header extends Component{
+	render(){
+		return(
+			<View style={[styles.header]}>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 01</Text>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 02</Text>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 03</Text>
+				<Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 04</Text>
 			</View>
 		);
 	}
@@ -86,6 +154,15 @@ const styles = StyleSheet.create({
 	body: {
 		flex: 1,
 		padding: 25,
+	},
+
+	input:{
+		height: 45,
+		borderWidth: 1,
+		borderColor: '#222',
+		padding: 10,
+		marginBottom: 10,
+		marginTop: 10
 	},
 
 	footer: {
